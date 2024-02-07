47b6cf09cfce0ee391fd35988fe143256f711e14
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-07 11:52:18 +0500

    enitity class of emplyee

---
+++ src/main/java/com/usman/ems/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.usman.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name="first-name")
+    private String firstName;
+
+    @Column(name="last-name")
+    private String lastName;
+
+    @Column(name = "email" , nullable = false , unique = true)
+    private String email;
+
+}

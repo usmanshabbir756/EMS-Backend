3100ce7cdce6d151af33b4751cf57910e1cda38a
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-07 12:37:22 +0500

    update mapper name

--- src/main/java/com/usman/ems/mapper/EmployeeMapper.java
+++ src/main/java/com/usman/ems/mapper/EmployeeMapper.java
@@ -4,14 +4,14 @@ import com.usman.ems.dto.EmployeeDto;
 import com.usman.ems.entity.Employee;
 
 public class EmployeeMapper {
-    public static EmployeeDto mapToEmployee(Employee employee){
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
         return new EmployeeDto(employee.getId(),
                     employee.getFirstName(),
                     employee.getLastName(),
                     employee.getEmail());
     }
 
-    public static Employee mapToEmployeeDto(EmployeeDto employeeDto){
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
         return new Employee(employeeDto.getId(),
                 employeeDto.getFirstName(),
                 employeeDto.getLastName(),
